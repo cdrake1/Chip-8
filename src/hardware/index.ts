9c7711cf56d6facb116dbbb2ac11de7d81d7a2a9
Collin Drakegit config --global user.email Collin.Drake1@marist.edugit config --global user.name Collin <Collin.Drake1@marist.edu>
2024-03-03 10:18:55 -0500

    looking at ways to get code to execute in browser

--- src/hardware/index.ts
+++ src/hardware/index.ts
@@ -1,7 +1,7 @@
 //This file will allow for function between the monitor and index.html
 
 //import monitor
-import { monitor } from "../src/hardware/monitor";
+import { monitor } from "./monitor";
 
 //create our monitor
 const _monitor = new monitor(0,"Monitor");
