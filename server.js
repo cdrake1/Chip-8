e98f5a9677388635054def25a1b535fc982f3b23
CFdefense <152815652+CFdefense@users.noreply.github.com>
2024-03-07 22:46:40 -0500

    updated commenting

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.use(express.static(path.join(__dirname, 'web')));
 app.use(express.static(path.join(__dirname, 'roms')));
 
-//route for server
+//route for server to serve as entry point for web application
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', './web/index.html'));
 });
 
-//start server and listen for HTTP requests
+//start server using port and listen for HTTP requests
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
