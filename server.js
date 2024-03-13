ec41a509d29738ab82bfb7a326039768cf5d4d81
Collin Drakegit config --global user.email Collin.Drake1@marist.edugit config --global user.name Collin <Collin.Drake1@marist.edu>
2024-03-13 15:58:54 -0400

    working on post request

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 const express = require('express'); //import express
 const app = express();  //creates an instance of express/server
-
-
 const path = require('path');   //module to assist in file directory/paths
 const PORT = process.env.PORT || 3000;  //checks if a port is available or defaults to port 3000
 
 //static files... make these directories available to the server
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use(express.static(path.join(__dirname, 'web')));
-app.use(express.static(path.join(__dirname, 'roms')));
 
 //route for server to serve as entry point for web application
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', './web/index.html'));
 });
+
+app.post('/uploadFile',function(req,res){
+    if(req.file){
+        res.send('File uploaded successfully!');
+    }
+    else{
+        res.status(404).send("An error occurred when uploading the file");
+    }
+})
 
 //start server using port and listen for HTTP requests
 app.listen(PORT, () => {
